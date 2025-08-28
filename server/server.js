03da61bf1e64f8cb668a23383dcd2ec19df757ab
Kamrul Hasan (ICT) <kamrul.babu@liz.com>
2025-08-28 13:36:26 +0600

    Add demo data and complete the API

--- server/server.js
+++ server/server.js
@@ -1,10 +1,14 @@
 import {ApolloServer} from "@apollo/server"
 import {startStandaloneServer} from "@apollo/server/standalone"
 
-
 const users = [
-    
-]
+  { id: 1, name: "John Doe", age: 28, isMarried: false },
+  { id: 2, name: "Alice Smith", age: 32, isMarried: true },
+  { id: 3, name: "Robert Brown", age: 40, isMarried: true },
+  { id: 4, name: "Sophia Johnson", age: 25, isMarried: false },
+  { id: 5, name: "Michael Lee", age: 35, isMarried: true }
+];
+
 
 
 const typeDefs = `
@@ -28,7 +32,28 @@ type User {
 `;
 
 const resolvers = {
+    Query: {
+        getUsers: () => {
 
+            return users;
+        },
+        getUserById: (parent, args) => {
+            const id = args.id;
+            return users.find((user) => user.id === id);
+        }
+    },
+    Mutation: {
+        createUser: (parent, arges) => {
+            const {name, age, isMarried} = args;
+            const newUser = {
+                id: (users.length + 1).toString(),
+                name,
+                age,
+                isMarried,
+            }
+            users.push(newUser)
+        }
+    },
 };
 
 const server = new ApolloServer({typeDefs, resolvers});
